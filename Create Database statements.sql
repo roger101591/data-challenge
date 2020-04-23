6a6cd57046bbd79645da0114fc7e5d8b48447f0b
roger101591 <rogertran123@gmail.com>
2020-04-23 11:09:53 -0700

    formatting..

--- Create Database statements.sql
+++ Create Database statements.sql
@@ -2,7 +2,7 @@ CREATE DATABASE if not exists dermstore_data_challenge;
 use dermstore_data_challenge;
 
 create table customers(
-	customer_id integer  not null auto_increment,
+    customer_id int  not null auto_increment,
     customer_firstname varchar(20),
     customer_lastname varchar(20),
     customer_email varchar(40),
@@ -11,12 +11,12 @@ create table customers(
     )
     
 ;
+#used integer datatypes to express ID fields for optimal join performance
 #decimal field length complies with Generally Accepted Accounting Principles (GAAP) rules src: https://www.mysqltutorial.org/mysql-decimal/
 
 create table products(
-	product_id integer not null auto_increment,
+    product_id int not null auto_increment,
     product_name varchar(40),
-
     product_price decimal(19,4),
     product_cost decimal(19,4),
     product_status varchar(20),
@@ -29,13 +29,11 @@ create table products(
 #changes in the customer_id in parent table `customers` will affect child table `orders` to ensure atomicity
 
 create table orders(
-	order_id integer not null auto_increment,
-    customer_id integer not null,
+    order_id int not null auto_increment,
+    customer_id int not null,
     order_total decimal(19,4),
-
     order_source TEXT,
     date_created datetime default current_timestamp,
-
     primary key (order_id),
     foreign key (customer_id)
 	references customers(customer_id)
@@ -47,9 +45,9 @@ create table orders(
 #changes in the customer_id in parent table `products` will affect child table `orders_items` to ensure atomicity
 
 create table orders_items(
-	order_id integer not null,
-    product_id integer not null,
-    item_quantity integer,
+    order_id int not null,
+    product_id int not null,
+    item_quantity int,
     item_price decimal(19,4),
     date_created datetime default current_timestamp, 
 	primary key(order_id, product_id),
